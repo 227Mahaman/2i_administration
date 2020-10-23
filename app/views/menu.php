e5c715ed3333f5c65576c9786b131ce90a337dcf
227Mahaman <zalkiabass.456@gmail.com>
2020-10-23 23:05:07 +0100

    menu delete lien en commentaire

--- app/views/menu.php
+++ app/views/menu.php
@@ -1,8 +1,4 @@
 <li class="active"><a href="index.php?p=dashboard"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
-<!--<li><a href="widgets.html"><em class="fa fa-calendar">&nbsp;</em> Widgets</a></li>
-<li><a href="charts.html"><em class="fa fa-bar-chart">&nbsp;</em> Charts</a></li>
-<li><a href="elements.html"><em class="fa fa-toggle-off">&nbsp;</em> UI Elements</a></li>
-<li><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Alerts &amp; Panels</a></li>-->
 <?php if(sizeof($_SESSION['bloc_administration'])!=0):?><!--Vérifier si l'user a accès au menu de ce bloc_administration-->
 <li class="parent "><a data-toggle="collapse" href="#sub-item-1">
     <em class="fa fa-navicon">&nbsp;</em> <?= $_SESSION['bloc_administration']['0']['libelle_groupe'];?> <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>

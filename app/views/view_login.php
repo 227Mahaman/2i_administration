a34e8ac1660cd88b41417542651230c49b8ece05
227Mahaman <zalkiabass.456@gmail.com>
2020-10-23 17:33:11 +0100

    add Logo

--- app/views/view_login.php
+++ app/views/view_login.php
@@ -19,7 +19,7 @@ $title="Authentification";
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">Log in</div>
+				<div class="panel-heading"><img src="" alt="2iSoft Logo"/></div>
 				<div class="panel-body">
 					<form role="form" method="post">
 						<fieldset>

3212663b964998bb27432f1d80b902e0a14edefd
227Mahaman <zalkiabass.456@gmail.com>
2020-10-23 22:10:51 +0100

    menu lien add commentaire

--- app/views/menu.php
+++ app/views/menu.php
@@ -1,8 +1,8 @@
 <li class="active"><a href="index.php?p=dashboard"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
-<li><a href="widgets.html"><em class="fa fa-calendar">&nbsp;</em> Widgets</a></li>
+<!--<li><a href="widgets.html"><em class="fa fa-calendar">&nbsp;</em> Widgets</a></li>
 <li><a href="charts.html"><em class="fa fa-bar-chart">&nbsp;</em> Charts</a></li>
 <li><a href="elements.html"><em class="fa fa-toggle-off">&nbsp;</em> UI Elements</a></li>
-<li><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Alerts &amp; Panels</a></li>
+<li><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Alerts &amp; Panels</a></li>-->
 <li class="parent "><a data-toggle="collapse" href="#sub-item-1">
     <em class="fa fa-navicon">&nbsp;</em> Multilevel <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
     </a>
